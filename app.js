147d6f6d8ae65fe9cb9e4b2f7fac7b33d8062391
Touahria Yacine <61922403+touahriayacine@users.noreply.github.com>
2023-04-27 12:32:55 +0000

    fixing bugs

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-
+const options = {
+    root: path.join(__dirname)
+};
 app.get("/", (req, res) => res.type("html").send(html));
-app.get("/arduino.json", (req, res) => res.sendFile("./arduino.json"));
-app.get("/app.json", (req, res) => res.sendFile("./app.json"));
+app.get("/arduino.json", (req, res) => res.sendFile("./arduino.json", options, function (err) {
+    if (err) {
+        next(err);
+    } else {
+        console.log('Sent:', "arduino.json");
+    }
+}));
+app.get("/app.json", (req, res) => res.sendFile("./app.json", options, function (err) {
+    if (err) {
+        next(err);
+    } else {
+        console.log('Sent:', "app.json");
+    }
+}));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const html = `
