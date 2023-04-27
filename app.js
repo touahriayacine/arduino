a96f80ad33aef8f1e40b811244d95833148d3edd
Touahria Yacine <61922403+touahriayacine@users.noreply.github.com>
2023-04-27 12:25:36 +0000

    adding features

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type("html").send(html));
 app.get("/arduino.json", (req, res) => res.sendFile("./arduino.json"));
+app.get("/app.json", (req, res) => res.sendFile("./app.json"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const html = `
